754c2ffc682c64e16edd0658ae9453aca29351ca
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2020-06-20 16:14:48 +0100

    PyQt5 web browser exploring.

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,22 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import *
 import os
 import sys
 import pygame
 
 current_path = os.getcwd()
 print(current_path)
+
+
+###### Trying QT Web #######
+
+app = QApplication(sys.argv)
+web = QWebEngineView()
+web.load(QUrl("https://www.nintendo.co.uk/"))
+
+
+#########
 
 
 #class Background(pygame.sprite.Sprite):
@@ -48,6 +61,14 @@ pygame.init()
 animation_increment = 10
 clock_tick_rate = 20
 
+
+black = (0, 0, 0)
+red = (200, 0, 0)
+green = (0, 200, 0)
+
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+
 # Open a window
 #size = (window_width, window_height)
 #screen = pygame.display.set_mode(size)
@@ -61,13 +82,45 @@ clock = pygame.time.Clock()
 #background_image = pygame.image.load(current_path+'/Images/artificial-intelligence-codes-developing-screen.jpg').convert()
 count = 0
 
+# Creating text
+def text_objects(text, font):
+    text_surface = font.render(text, True, black)
+    return text_surface, text_surface.get_rect()
+
+
+def button(msg, x, y, w, h, ic, ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac, (x, y, w, h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(screen, ic, (x, y, w, h))
+
+    smallText = pygame.font.SysFont("comicsansms", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x+(w/2)), (y+(h/2)))
+    screen.blit(textSurf, textRect)
+
+
+def show_web_page():
+    web.show()
+    # sys.exit(app.exec_())  -> Figure out a better way to close the application
+
+
 def menu():
+    
     pygame.display.set_caption("Main Menu")
-    count = 0
-    dead = False
     window_width = 1024
     window_height = 640
-    screen = screen = set_screen_size(window_width, window_height)
+    screen = set_screen_size(window_width, window_height)
+
+    count = 0
+    dead = False
+    
     background_image = pygame.image.load(current_path+'/Images/AI Brain.jpg').convert()
     #background_image = pygame.transform.scale(background_image, (1024,640))
 
@@ -78,7 +131,29 @@ def menu():
 
         screen.blit(background_image, [0, 0])
 
-        pygame.display.flip()
+        mouse = pygame.mouse.get_pos()
+
+        #print(mouse)
+
+        if 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
+            pygame.draw.rect(screen, bright_green, (150, 450, 100, 50))
+        else:
+            pygame.draw.rect(screen, green, (150, 450, 100, 50))
+
+        button("GO!", 150, 450, 100, 50, green, bright_green, splash_screen)
+        button("Quit", 550, 450, 100, 50, red, bright_red, show_web_page)
+
+        '''
+        # Creating buttons
+        #pygame.draw.rect(screen, (0, 255, 0), (150, 450, 100, 50))
+        pygame.draw.rect(screen, (255, 0, 0), (550, 450, 100, 50)) # ('screen to place',('Colour'),(location))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects("Button 1!", smallText)
+        textRect.center = ((150+(100/2)), (450+(50/2)))
+        screen.blit(textSurf, textRect)
+        '''
+        pygame.display.update()
         clock.tick(clock_tick_rate)
         count += 1
 
@@ -90,10 +165,10 @@ def set_screen_size(width,height):
 def splash_screen():
     count = 0
     dead = False
-    window_width = 1024
-    window_height = 660
+    window_width = 910
+    window_height = 607
     #size = (window_width, window_height)
-    screen = set_screen_size(window_width,window_height)
+    set_screen_size(window_width,window_height)
     background_image = pygame.image.load(current_path + '/Images/artificial-intelligence-codes-developing-screen.jpg').convert()
     while(dead == False):
         for event in pygame.event.get():
@@ -102,13 +177,13 @@ def splash_screen():
 
         screen.blit(background_image, [0, 0])
 
-        pygame.display.flip()
+        pygame.display.update()
         clock.tick(clock_tick_rate)
         count += 1
         if count == 10:
-            break
+            menu()
 
+screen = set_screen_size(200,200)
 splash_screen()
-menu()
 
 print("I am out!")
