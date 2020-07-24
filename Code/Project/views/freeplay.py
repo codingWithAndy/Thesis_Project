9393b541ca303e398ec8decdfa3ba2ad93a59921
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2020-07-24 10:52:06 +0100

    Free Play update & added data samples for Lin Reg.

--- Code/Project/views/freeplay.py
+++ Code/Project/views/freeplay.py
@@ -135,6 +135,7 @@ class FreePlay(QtWidgets.QWidget):
         self.modelInfoLayoutV.addWidget(self.frame)
         self.gamboardModelInforLayoutV.addLayout(self.modelInfoLayoutV)
         self.verticalLayout_2.addLayout(self.gamboardModelInforLayoutV)
+
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.groupBox = QtWidgets.QGroupBox(self)
@@ -144,6 +145,195 @@ class FreePlay(QtWidgets.QWidget):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(
             self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setMinimumSize(QtCore.QSize(700, 210))
+        self.groupBox.setMaximumSize(QtCore.QSize(16777215, 600))
+        self.groupBox.setStyleSheet("background-color: rgb(195, 192, 44);")
+        self.groupBox.setObjectName("groupBox")
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout(self.groupBox)
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.groupBoxH1 = QtWidgets.QHBoxLayout()
+        self.groupBoxH1.setObjectName("groupBoxH1")
+        self.generalGroupbox = QtWidgets.QGroupBox(self.groupBox)
+        self.generalGroupbox.setObjectName("generalGroupbox")
+        self.formLayout_3 = QtWidgets.QFormLayout(self.generalGroupbox)
+        self.formLayout_3.setObjectName("formLayout_3")
+
+        self.modelLabel = QtWidgets.QLabel(self.generalGroupbox)
+        self.modelLabel.setObjectName("modelLabel")
+        self.formLayout_3.setWidget(
+            0, QtWidgets.QFormLayout.LabelRole, self.modelLabel)
+
+        self.comboBox = QtWidgets.QComboBox(self.generalGroupbox)
+        self.comboBox.setStyleSheet("background-color: white;"
+                                    "selection-color: rgb(200, 200, 200)")
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.formLayout_3.setWidget(
+            0, QtWidgets.QFormLayout.FieldRole, self.comboBox)
+
+        self.numberOfClustersLabel = QtWidgets.QLabel(self.generalGroupbox)
+        self.numberOfClustersLabel.setObjectName("numberOfClustersLabel")
+        self.formLayout_3.setWidget(
+            1, QtWidgets.QFormLayout.LabelRole, self.numberOfClustersLabel)
+
+        self.numberOfClustersLineedit = QtWidgets.QLineEdit(
+            self.generalGroupbox)
+        self.numberOfClustersLineedit.setMaximumSize(
+            QtCore.QSize(30, 16777215))
+        self.numberOfClustersLineedit.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.numberOfClustersLineedit.setAutoFillBackground(False)
+        self.numberOfClustersLineedit.setStyleSheet("background-color: white;\n"
+                                                    "")
+        self.numberOfClustersLineedit.setObjectName("lineEdit")
+        self.formLayout_3.setWidget(
+            1, QtWidgets.QFormLayout.FieldRole, self.numberOfClustersLineedit)
+
+        self.boundaryLabel = QtWidgets.QLabel(self.generalGroupbox)
+        self.boundaryLabel.setObjectName("boundaryLabel")
+        self.formLayout_3.setWidget(
+            3, QtWidgets.QFormLayout.LabelRole, self.boundaryLabel)
+
+        self.boundaryOnOffRadioButton = QtWidgets.QRadioButton(
+            self.generalGroupbox)
+        self.boundaryOnOffRadioButton.setMaximumSize(
+            QtCore.QSize(25, 16777215))
+        self.boundaryOnOffRadioButton.setText("")
+        self.boundaryOnOffRadioButton.setObjectName("boundaryOnOffRadioButton")
+        self.formLayout_3.setWidget(
+            3, QtWidgets.QFormLayout.FieldRole, self.boundaryOnOffRadioButton)
+
+        self.dataSampleLabel = QtWidgets.QLabel(self.generalGroupbox)
+        self.dataSampleLabel.setObjectName("label")
+        self.formLayout_3.setWidget(
+            2, QtWidgets.QFormLayout.LabelRole, self.dataSampleLabel)
+
+        self.dataSampleLineedit = QtWidgets.QLineEdit(self.generalGroupbox)
+        self.dataSampleLineedit.setMinimumSize(QtCore.QSize(30, 0))
+        self.dataSampleLineedit.setMaximumSize(QtCore.QSize(30, 16777215))
+        self.dataSampleLineedit.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.dataSampleLineedit.setAutoFillBackground(False)
+        self.dataSampleLineedit.setStyleSheet("background-color: white;")
+        self.dataSampleLineedit.setObjectName("lineEdit_2")
+        self.formLayout_3.setWidget(
+            2, QtWidgets.QFormLayout.FieldRole, self.dataSampleLineedit)
+        self.groupBoxH1.addWidget(self.generalGroupbox)
+        self.horizontalLayout_5.addLayout(self.groupBoxH1)
+        self.groupBoxH2 = QtWidgets.QHBoxLayout()
+        self.groupBoxH2.setObjectName("groupBoxH2")
+        self.dataGroupbox = QtWidgets.QGroupBox(self.groupBox)
+        self.dataGroupbox.setMinimumSize(QtCore.QSize(200, 0))
+        self.dataGroupbox.setObjectName("dataGroupbox")
+        self.formLayout = QtWidgets.QFormLayout(self.dataGroupbox)
+        self.formLayout.setObjectName("formLayout")
+        self.data1Label = QtWidgets.QLabel(self.dataGroupbox)
+        self.data1Label.setObjectName("data1Label")
+        self.formLayout.setWidget(
+            0, QtWidgets.QFormLayout.LabelRole, self.data1Label)
+        self.data2Label = QtWidgets.QLabel(self.dataGroupbox)
+        self.data2Label.setObjectName("data2Label")
+        self.formLayout.setWidget(
+            1, QtWidgets.QFormLayout.LabelRole, self.data2Label)
+        self.data1RadioBtn = QtWidgets.QRadioButton(self.dataGroupbox)
+        self.data1RadioBtn.setText("")
+        self.data1RadioBtn.setObjectName("data1RadioBtn")
+        self.formLayout.setWidget(
+            0, QtWidgets.QFormLayout.FieldRole, self.data1RadioBtn)
+        self.data2RadioBtn = QtWidgets.QRadioButton(self.dataGroupbox)
+        self.data2RadioBtn.setText("")
+        self.data2RadioBtn.setObjectName("data2RadioBtn")
+        self.formLayout.setWidget(
+            1, QtWidgets.QFormLayout.FieldRole, self.data2RadioBtn)
+        self.data3Label = QtWidgets.QLabel(self.dataGroupbox)
+        self.data3Label.setObjectName("data3Label")
+        self.formLayout.setWidget(
+            2, QtWidgets.QFormLayout.LabelRole, self.data3Label)
+        self.data3radioBtn = QtWidgets.QRadioButton(self.dataGroupbox)
+        self.data3radioBtn.setText("")
+        self.data3radioBtn.setObjectName("data3radioBtn")
+        self.formLayout.setWidget(
+            2, QtWidgets.QFormLayout.FieldRole, self.data3radioBtn)
+        self.groupBoxH2.addWidget(self.dataGroupbox)
+        self.horizontalLayout_5.addLayout(self.groupBoxH2)
+        self.groupBoxH3 = QtWidgets.QHBoxLayout()
+        self.groupBoxH3.setObjectName("groupBoxH3")
+        spacerItem2 = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.groupBoxH3.addItem(spacerItem2)
+        self.horizontalLayout_5.addLayout(self.groupBoxH3)
+        self.groupBoxH4 = QtWidgets.QHBoxLayout()
+        self.groupBoxH4.setObjectName("groupBoxH4")
+        spacerItem3 = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.groupBoxH4.addItem(spacerItem3)
+        self.horizontalLayout_5.addLayout(self.groupBoxH4)
+        self.groupBoxHButtons = QtWidgets.QHBoxLayout()
+        self.groupBoxHButtons.setObjectName("groupBoxHButtons")
+        self.buttonLayoutV = QtWidgets.QVBoxLayout()
+        self.buttonLayoutV.setObjectName("buttonLayoutV")
+        spacerItem4 = QtWidgets.QSpacerItem(
+            20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.buttonLayoutV.addItem(spacerItem4)
+
+        self.playButton = QtWidgets.QPushButton(self.groupBox)
+        self.playButton.setMinimumSize(QtCore.QSize(101, 51))
+        self.playButton.setStyleSheet("background-color: rgb(3, 193, 161);\n"
+                                      "border-radius: 15px;")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(
+            self.current_path+"/Code/Project/Images/play-circle-regular.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.playButton.setIcon(icon)
+        self.playButton.setIconSize(QtCore.QSize(40, 40))
+        self.playButton.setObjectName("playButton")
+        self.buttonLayoutV.addWidget(self.playButton)
+
+        self.clearButton = QtWidgets.QPushButton(self.groupBox)
+        self.clearButton.setMinimumSize(QtCore.QSize(101, 51))
+        self.clearButton.setStyleSheet("background-color: rgb(3, 193, 161);\n"
+                                       "border-radius: 15px;")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(
+            self.current_path+"/Code/Project/Images/times-circle-solid.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.clearButton.setIcon(icon1)
+        self.clearButton.setIconSize(QtCore.QSize(40, 40))
+        self.clearButton.setObjectName("clearButton")
+        self.buttonLayoutV.addWidget(self.clearButton)
+
+        self.homeButton = QtWidgets.QPushButton(self.groupBox)
+        self.homeButton.setMinimumSize(QtCore.QSize(101, 51))
+        self.homeButton.setStyleSheet("background-color: rgb(3, 193, 161);\n"
+                                      "border-radius: 15px;")
+
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(self.current_path+"/Code/Project/Images/home-solid.svg"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.homeButton.setIcon(icon2)
+        self.homeButton.setIconSize(QtCore.QSize(40, 30))
+        self.homeButton.setObjectName("homeButton")
+        self.buttonLayoutV.addWidget(self.homeButton)
+        self.groupBoxHButtons.addLayout(self.buttonLayoutV)
+        self.horizontalLayout_5.addLayout(self.groupBoxHButtons)
+        self.verticalLayout.addWidget(self.groupBox)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        
+        '''
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        
+        
+        self.groupBox = QtWidgets.QGroupBox(self)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.groupBox.sizePolicy().hasHeightForWidth())
+        
         self.groupBox.setSizePolicy(sizePolicy)
         self.groupBox.setMinimumSize(QtCore.QSize(700, 210))
         self.groupBox.setMaximumSize(QtCore.QSize(16777215, 600))
@@ -181,14 +371,14 @@ class FreePlay(QtWidgets.QWidget):
         self.numberOfClustersLabel.setObjectName("numberOfClustersLabel")
         self.formLayout_3.setWidget(
             1, QtWidgets.QFormLayout.LabelRole, self.numberOfClustersLabel)
-        self.lineEdit = QtWidgets.QLineEdit(self.generalLayoutG)
-        self.lineEdit.setMaximumSize(QtCore.QSize(30, 16777215))
-        self.lineEdit.setAutoFillBackground(False)
-        self.lineEdit.setStyleSheet("background-color: white;\n"
-                                    "")
-        self.lineEdit.setObjectName("lineEdit")
+        self.numberOfClustersLineedit = QtWidgets.QLineEdit(self.generalLayoutG)
+        self.numberOfClustersLineedit.setMaximumSize(QtCore.QSize(30, 16777215))
+        self.numberOfClustersLineedit.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.numberOfClustersLineedit.setAutoFillBackground(False)
+        self.numberOfClustersLineedit.setStyleSheet("background-color: white;")
+        self.numberOfClustersLineedit.setObjectName("lineEdit")
         self.formLayout_3.setWidget(
-            1, QtWidgets.QFormLayout.FieldRole, self.lineEdit)
+            1, QtWidgets.QFormLayout.FieldRole, self.numberOfClustersLineedit)
         self.boundaryLabel = QtWidgets.QLabel(self.generalLayoutG)
         self.boundaryLabel.setObjectName("boundaryLabel")
         self.formLayout_3.setWidget(
@@ -214,12 +404,15 @@ class FreePlay(QtWidgets.QWidget):
         self.horizontalLayout_5.addLayout(self.groupBoxH4)
         self.groupBoxHButtons = QtWidgets.QHBoxLayout()
         self.groupBoxHButtons.setObjectName("groupBoxHButtons")
+        
+        # Button Layout container
         self.buttonLayoutV = QtWidgets.QVBoxLayout()
         self.buttonLayoutV.setObjectName("buttonLayoutV")
         spacerItem2 = QtWidgets.QSpacerItem(
             20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.buttonLayoutV.addItem(spacerItem2)
         
+        # Buttons Created
         self.playButton = QtWidgets.QPushButton(self.groupBox)
         self.playButton.setMinimumSize(QtCore.QSize(101, 51))
         self.playButton.setStyleSheet("background-color: rgb(3, 193, 161);\n"
@@ -258,10 +451,13 @@ class FreePlay(QtWidgets.QWidget):
         self.homeButton.setObjectName("homeButton")
         self.buttonLayoutV.addWidget(self.homeButton)
         
+
         self.groupBoxHButtons.addLayout(self.buttonLayoutV)
         self.horizontalLayout_5.addLayout(self.groupBoxHButtons)
         self.verticalLayout.addWidget(self.groupBox)
         self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        '''
 
         # Button Connects
         self.playButton.clicked.connect(self.update_graph)
@@ -283,7 +479,7 @@ class FreePlay(QtWidgets.QWidget):
         self.modelTypeLabel.setText(_translate("self", "Model name"))
         self.overviewLabel.setText(_translate("self", "Overview:"))
         self.groupBox.setTitle(_translate("self", "Model Options:"))
-        self.generalLayoutG.setTitle(_translate("self", "General:"))
+        self.generalGroupbox.setTitle(_translate("self", "General:"))
         self.modelLabel.setText(_translate("self", "Model:"))
         self.numberOfClustersLabel.setText(
             _translate("self", "No. of Clusters:"))
@@ -294,6 +490,14 @@ class FreePlay(QtWidgets.QWidget):
         self.comboBox.setItemText(3, _translate("Form", "Linear Regression"))
         self.comboBox.setItemText(4, _translate("Form", "GMM"))
         self.comboBox.setItemText(5, _translate("Form", "SVM"))
+
+        self.dataSampleLabel.setText(_translate("self", "No. data samples:"))
+        self.dataGroupbox.setTitle(_translate("self", "Pre-Generated Data:"))
+        self.data1Label.setText(_translate("self", "House Prices:"))
+        self.data2Label.setText(_translate("self", "Health (Diabetes):"))
+        self.data3Label.setText(_translate("self", "OG Created Data:"))
+
+
 
         self.overviewDescriptionLabel.setText(_translate(
             "Form", self.MplWidget.model_overview))
@@ -344,6 +548,9 @@ class FreePlay(QtWidgets.QWidget):
 
     def handleActivated(self, index):
         print(self.comboBox.itemText(index))
+        if self.boundaryOnOffRadioButton.isChecked() == True:
+            self.boundaryOnOffRadioButton.setChecked(False)
+        
         if self.comboBox.itemText(index) == "LDA":
             self.game_mode = "lda"
             self.MplWidget.hide()
--- Code/Project/views/linearregressiongameboard.py
+++ Code/Project/views/linearregressiongameboard.py
@@ -10,6 +10,9 @@ import matplotlib
 
 ##
 from sklearn.linear_model import LinearRegression
+from sklearn import datasets
+
+from random import randint
 
 matplotlib.use('Qt5Agg')
 
@@ -17,10 +20,16 @@ matplotlib.use('Qt5Agg')
 class LinearRegressionGameboard(QWidget):
     model_name = "Linear Regression"
     learning_type = "Supervised Learning!"
-    model_overview = """Linear regression attempts to model the relationship between two variables by fitting a linear equation to observed data. One variable is considered to be an explanatory variable, and the other is considered to be a dependent variable. For example, a modeler might want to relate the weights of individuals to their heights using a linear regression model."""
+    model_overview = "Linear regression attempts to model the relationship between two variables by fitting a linear equation to observed data." + \
+                     " One variable is considered to be an explanatory variable, and the other is considered to be a dependent variable." + \
+                     " For example, a modeler might want to relate the weights of individuals to their heights using a linear regression model."
 
-    X = 2 * np.random.rand(100, 1)
-    y = 4+3 * X + np.random.randn(100, 1)
+    data_sample_1 = datasets.load_diabetes()
+    data_sample_2 = datasets.load_boston()
+
+    
+
+    lin_reg = LinearRegression()
 
     boundaries_on = False
 
@@ -30,6 +39,7 @@ class LinearRegressionGameboard(QWidget):
         QWidget.__init__(self, parent)
 
         self.canvas = FigureCanvas(Figure())
+        self.data_option = randint(0, 1)
 
         self.vertical_layout = QVBoxLayout()
         self.vertical_layout.addWidget(self.canvas)
@@ -38,91 +48,43 @@ class LinearRegressionGameboard(QWidget):
         self.canvas.ax = self.fig.add_subplot(111)
 
         # create data points
-        self.lin_reg = LinearRegression()
-        self.lin_reg.fit(self.X, self.y)
+        self.generate_data_points()
+        self.fit_data_points()
+        
+        self.fig.canvas.draw()
 
-        self.X_new = np.array([[0], [2]])
+        self.setLayout(self.vertical_layout)
+ 
+ 
+
+    def generate_data_points(self):
+        print("Data option:",self.data_option)
+        if self.data_option == 1:
+            self.X, self.y = datasets.load_diabetes(return_X_y=True)
+            self.X = self.X[:, np.newaxis, 2]
+            self.X_new = self.X
+            self.fit_data_points()
+        else:
+            self.X = 2 * np.random.rand(100, 1)
+            self.y = 4+3 * self.X + np.random.randn(100, 1)
+            self.X_new = np.array([[0], [2]])
+            self.fit_data_points()
+
+    def fit_data_points(self):
+        self.lin_reg.fit(self.X, self.y)
+        self.canvas.ax.plot(self.X, self.y, "b.")
+        
+        # Creating Predictions
+        
         self.y_pred = self.lin_reg.predict(self.X_new)
 
-        
-        self.canvas.ax.plot(self.X, self.y, "b.")
-        #self.canvas.ax..axis([0, 2, 0, 15])
-
-
-        self.fig.canvas.draw()
-
-        #self.plot_clusters(self.X)
+    #def __call__(self, event):
+    #    print('click', event)
+    #    self.kmeans = KMeans(n_clusters=self.k, random_state=42)
+    #    self.y_pred = self.kmeans.fit_predict(self.X)
+    #    self.plot_decision_boundaries(self.kmeans, self.X)
 #
-        self.setLayout(self.vertical_layout)
-        #self.cid = self.canvas.figure.canvas.mpl_connect(
-        #    'button_press_event', self)
-
-    def plot_clusters(self, X, y=None):
-        self.canvas.ax.scatter(X[:, 0], X[:, 1], c=y, s=1)
-        self.fig.canvas.draw()
-
-    def __call__(self, event):
-        print('click', event)
-        self.kmeans = KMeans(n_clusters=self.k, random_state=42)
-        self.y_pred = self.kmeans.fit_predict(self.X)
-        self.plot_decision_boundaries(self.kmeans, self.X)
-
         # Plot new data points prediction.
-
-    def plot_data(self, X):
-        self.canvas.ax.plot(self.X[:, 0], self.X[:, 1], 'k.', markersize=2)
-
-    def plot_centroids(self, centroids, weights=None, circle_color='w', cross_color='k'):
-        if weights is not None:
-            centroids = centroids[weights > weights.max() / 10]
-        self.canvas.ax.scatter(centroids[:, 0], centroids[:, 1],
-                               marker='o', s=30, linewidths=8,
-                               color=circle_color, zorder=10, alpha=0.9
-                               )
-        self.canvas.ax.scatter(centroids[:, 0], centroids[:, 1],
-                               marker='x', s=50, linewidths=50,
-                               color=cross_color, zorder=11, alpha=1)
-
-    def plot_decision_boundaries(self, clusterer, X, resolution=1000, show_centroids=True,
-                                 show_xlabels=True, show_ylabels=True):
-        mins = X.min(axis=0) - 0.1
-        maxs = X.max(axis=0) + 0.1
-        xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], resolution),
-                             np.linspace(mins[1], maxs[1], resolution))
-        Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z.reshape(xx.shape)
-
-        self.canvas.ax.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
-                                cmap="Pastel2")
-        self.canvas.ax.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
-                               linewidths=1, colors='k')
-        self.plot_data(self.X)
-        if show_centroids:
-            self.plot_centroids(clusterer.cluster_centers_)
-
-        self.fig.canvas.draw()
-
-    # Experimenting
-
-    def replot_kmeans(self):
-        print(self.k)
-        self.canvas.ax.clear()
-        self.blob_centers = np.array(
-            [[0.2,  2.3],
-             [-1.5,  2.3],
-             [-2.8,  1.3]
-             ]
-        )
-        self.blob_std = np.array([0.4, 0.3,
-                                  0.1]
-                                 )
-        self.X, self.y = make_blobs(n_samples=2000, centers=self.blob_centers,
-                                    cluster_std=self.blob_std, random_state=7)
-
-        self.plot_clusters(self.X)
-        self.fig.canvas.draw()
-
-    boundaries_on = False
 
     # Toggle decision boundary off
     def switch_boundaries_on_off(self):
